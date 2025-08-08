b2bb6c537e2472baed7664d8b80224663dc04674
Tomlena-tech <decourttthomas@gmail.com>
2025-08-08 12:56:51 +0200

    Add settings sort.h betty's style

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,9 +1,9 @@
 #include "sort.h"
 /**
  * bubble_sort - Sorts an array of integers in ascending order using
- * the Bubble sort algorithm.
- * @array: The array to be sorted.
- * @size: The number of elements in the array.
+ * the Bubble sort algorithm
+ * @array: The array to be sorted
+ * @size: The number of elements in the array
  */
 void bubble_sort(int *array, size_t size)
 {
--- sort.h
+++ sort.h
@@ -1,9 +1,10 @@
+#ifndef SORT_H
+#define SORT_H
 /* sort.h
  * Header file for sorting algorithms
  * This file contains the declarations of sorting functions and data structures.
  */
-#ifndef SORT_H
-#define SORT_H
+
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,9 +12,9 @@
 
 typedef struct listint_s
 {
-	int n;
-	struct listint_s *prev;
-	struct listint_s *next;
+int n;
+struct listint_s *prev;
+struct listint_s *next;
 } listint_t;
 
 
